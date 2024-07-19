bf9bdd486b17ff9a9a8f784862f651dbaabea490
Dixit Rathi <121105687+dixitrathi7@users.noreply.github.com>
2024-07-19 12:49:03 +0530

    Update and rename var.tf to variable.tf

--- aws-ec2-inst/variable.tf
+++ aws-ec2-inst/variable.tf
@@ -1,21 +1,24 @@
+variable "region" {
+  description = "i use us-east-1 region"
+}
 
 variable "inst_count" {
   description = "Number of instances to launch"
-  default     = 2
   type        = number
 }
 
 variable "inst_ami" {
   description = "AMI ID for the instance"
-  default     = "ami-0b72821e2f351e396"
 }
 
 variable "inst_type" {
   description = "Instance type for the EC2 instance"
-  default     = "t2.micro"
+}
+
+variable "inst_security_group" {
+  description = "Security group ID for the instance"
 }
 
 variable "inst_pem_key" {
   description = "Name of the PEM key pair used for SSH access"
-  default     = "dixit-n-virg"
 }
