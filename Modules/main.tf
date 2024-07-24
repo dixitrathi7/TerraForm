9956d20f9f7791eb15ba9e3d457cfcb3ef8e4377
Dixit Rathi <121105687+dixitrathi7@users.noreply.github.com>
2024-07-24 09:24:03 +0530

    Update main.tf

--- Modules/main.tf
+++ Modules/main.tf
@@ -18,11 +18,3 @@ module "nat_module" {
   route_table_id = module.launch_vpc.Private_route_table_id
   subnet_id      = module.launch_vpc.public_subnet_id
 }
-
-module "launch_ec2" {
-    source = ".//EC2_module"
-    inst_pem_key  = var.inst_pem_key  
-    inst_subnet_id = var.inst_subnet_id
-
-}
-
